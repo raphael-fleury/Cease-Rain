6870a2c9ffde6a1f70a9d63a9177262fbb5691d7
Raphael Fleury <raphael.fleury9@gmail.com>
2019-12-13 15:31:06 -0300

    Added Rain Maker asset

---
+++ Assets/Scripts/Objects/Scenery/Puddle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puddle : MonoBehaviour
+{
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //if (collision.gameObject.GetComponent<Rigidbody2D>().velocity.x != 0)
+
+    }
+
+}
